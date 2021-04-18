e3c94779acc14943150d5c9a171073851507ac66
Duoc Ngo <nhd.ars@gmail.com>
2021-04-18 10:08:29 +0700

    remove tailwinds

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {},
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
